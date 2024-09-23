8e249a62144e1236d09b3b8b7d4d08ddd934f0ae
itsgitz <anggit.ginanjar.dev@gmail.com>
2024-09-23 17:09:45 +0700

    chore: bar

--- lambda/index.ts
+++ lambda/index.ts
@@ -1,9 +1,11 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/aws-lambda'
 import { formatInTimeZone } from 'date-fns-tz'
+import {compareAsc} from 'date-fns'
 
 const app = new Hono()
-const expectedDate = '2024-09-23T23:00:00.000Z'
+//const expectedDate = '2024-09-23T23:00:00.000Z'
+const expectedDate = '2024-09-23T09:00:00.000Z'
 
 function generateTimestampNow() {
   return new Date().toISOString()
@@ -11,13 +13,16 @@ function generateTimestampNow() {
 
 app.get('/', async (c) => {
   const disableMeetingAt = formatInTimeZone(expectedDate, 'Asia/Makassar', 'MMMM d, yyyy HH:mm')
-  const dateTime = generateTimestampNow()
-  const dateTimeTz = formatInTimeZone(dateTime, 'Asia/Makassar', 'MMMM d, yyyy HH:mm')
+  const dateNow = generateTimestampNow()
+  const dateNowTz = formatInTimeZone(dateNow, 'Asia/Makassar', 'MMMM d, yyyy HH:mm')
+
+  const isDisabled = (new Date(dateNowTz) > new Date(disableMeetingAt))
 
   return c.json({
-    dateTime,
-    dateTimeTz,
+    dateNow,
+    dateNowTz,
     disableMeetingAt,
+    isDisabled,
     message: 'anggit adalah lambda'
   })
 })
